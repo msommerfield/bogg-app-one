e4915ee9b3c5e3c9989d74b2a2ba7a55d52975bc
Mandy <mandysommerfield@gmail.com>
2019-03-19 10:42:53 -0400

    set up dotenv and server files

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const logger = require('morgan')
+const app = express();
+
+app.use(express.json());
+app.get('/', (req,res) => {
+  res.send('Hello world!')
+})
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Magic happening on port " + ${PORT}`);
+})
